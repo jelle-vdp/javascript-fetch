6a3feada8672aa20173e3ba744e143d3ca2cffeb
jelle-vdp <jellevandepoel@gmail.com>
2022-05-09 14:42:33 +0200

    finished first exercise

---
+++ 01-list-to-console/script.js
@@ -0,0 +1,21 @@
+/* becode/javascript
+ *
+ * /09-fetch/01-list-to-console/script.js - 11.1: liste vers console
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    
+    document.getElementById("run").addEventListener("click", () => {
+        fetch("../../_shared/api.json")
+            .then(res => res.json())
+            .then(data => console.log(data));
+    })
+
+    
+})();
