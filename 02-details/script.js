a09448d52d46fe2de1fc660152fbf2320edba650
jelle-vdp <jellevandepoel@gmail.com>
2022-05-10 09:46:59 +0200

    finished exercise two

--- 02-details/script.js
+++ 02-details/script.js
@@ -9,6 +9,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (() => {
-    // your code here
+
+    document.getElementById("run").addEventListener("click", () => {
+        fetch("../../_shared/api.json")
+            .then(res => res.json())
+            .then(data => {
+                
+                const heroId = +document.getElementById("hero-id").value;
+
+                const hero = data.heroes.find(heroEl => heroEl.id === heroId);
+                console.log(hero);
+
+                const templateEl = document.getElementById("tpl-hero");
+                let clone = templateEl.content.cloneNode(true);
+
+                const nameEl = clone.querySelector(".name")
+                const alterEgoEl = clone.querySelector(".alter-ego");
+                const powersEl = clone.querySelector(".powers");
+
+                nameEl.innerText = hero.name;
+                alterEgoEl.innerText = hero.alterEgo;
+                powersEl.innerText = hero.abilities.join(', ');
+
+                document.getElementById("target").appendChild(clone);
+
+            });
+    });
+
 })();
